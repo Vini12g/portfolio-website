a3d7e88456d1dce71440fa3cdbcd77805b42b1b5
André Vinicius Guimarães <106218637+Vini12g@users.noreply.github.com>
2024-03-11 13:02:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight; 
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navbar.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+            });
+        }
+    });
+    
+    // Sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100); 
+}
